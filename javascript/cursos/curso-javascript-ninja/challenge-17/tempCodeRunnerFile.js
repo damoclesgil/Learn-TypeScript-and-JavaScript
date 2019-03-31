5ba5df8e481756a63a67018a03ecc161e9af13a1
damoclesgil <damoclesgil@gmail.com>
2019-03-31 16:56:51 -0300

    :D

---
+++ javascript/cursos/curso-javascript-ninja/challenge-17/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+let text = 'Manuel Marques de Sousa, Conde de Porto Alegre(Rio Grande, 13 de junho de 1804– Rio de Janeiro, 18 de julho de 1875), apelidado de "O Centauro de Luvas", foi um militar, político, abolicionista e monarquista brasileiro.'
+
+/*
+Vamos começar com umas brincadeiras fáceis :D
+Troque o nome "Manuel Marques de Souza" pelo seu nome, e mostre o resultado
+no console:
+*/
+console.log('Adicionando seu nome no texto:');
+// ?
+console.logo(text.replace(/Manuel Marques de Sousa/, 'Dâmocles Gil Marçal'))
